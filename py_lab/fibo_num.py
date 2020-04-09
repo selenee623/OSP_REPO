af34eb82795eb9339fd63964a7032d8749052837
selenee623 <62587855+selenee623@users.noreply.github.com>
2020-04-09 15:21:29 +0900

    Update fibo_num.py

--- py_lab/fibo_num.py
+++ py_lab/fibo_num.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-n = int(input("fibonacci number? "))
+n = int(input("fibonacci number? "))      #1
 
 def fibo(n):
   list = []
   for i in range(0, n):
     if i < 2:
-      list.append(1)
+      list.append(1)                      #F1 = F2 = 1
     else:
-      list.append(list[i-1] + list[i-2])
-  print(", ".join(map(str, list)))
+      list.append(list[i-1] + list[i-2])  #F(n) = F(n-1) + F(n-2)
+  print(", ".join(map(str, list)))        #2
   return list[n-1]
 
-print("F%d = %d" % (n, fibo(n)))
+print("F%d = %d" % (n, fibo(n)))          #3
